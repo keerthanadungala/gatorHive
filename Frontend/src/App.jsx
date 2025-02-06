1b70992a350a4a17ca1d50229d20236233f83b71
Akashchalla <challaakash505@gmail.com>
2025-02-05 22:45:16 -0500

    added event creation page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import EventCreate from "./pages/EventCreate";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create" element={<EventCreate />} />
       </Routes>
     </Router>
   );
