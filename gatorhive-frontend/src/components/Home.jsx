1efa5bf7601139c53b49b3c8145dd1ae045769fd
harsha230 <harshavardhanvc123@gmail.com>
2025-02-05 20:48:06 -0500

    Added gatorhive-frontend as a normal folder

---
+++ gatorhive-frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <div className="home">
+        <h2>Welcome to GatorHive</h2>
+        <p>Your go-to platform for discovering and organizing events at the University of Florida.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
