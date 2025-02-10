c5d150d49a180e0f118f526e8d198ee04af9e8dd
Akashchalla <96239061+Akashchalla@users.noreply.github.com>
2025-02-10 08:00:47 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,14 +1,5 @@
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // Modern CSS for Navbar
-
-
-
-import "./Navbar.css"; // Modern CSS for Navbar
-
-import "./Navbar.css"; // Modern UF-Themed Navbar Styles
-
-
-
+import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -16,16 +7,7 @@ const Navbar = () => {
       <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/create">Create Event</Link>
-
-
-
-
-
-
         <Link to="/events">View Events</Link>
-
-
-
       </div>
     </nav>
   );
